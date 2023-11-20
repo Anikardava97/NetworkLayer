99aed439dac29f02739623bd524a9be0b43a44b0
Anikardava <anikardava97@gmail.com>
2023-11-20 00:08:55 -0800

    first commit

---
+++ Sources/NetworkLayer/NetworkLayer.swift
@@ -0,0 +1,45 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+protocol DecodableModel: Decodable {}
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+    case other(Error)
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+
+    private init() {}
+
+    func fetch<T: DecodableModel>(from urlString: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(.other(error)))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
